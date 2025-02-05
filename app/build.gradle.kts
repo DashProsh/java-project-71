fd4830355d8400e022714859bac40d487a55f7c3
DashProsh <dash.prosh.am@gmail.com>
2025-02-05 16:52:41 +0200

    added Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,11 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-    id("java")
+    id("application")
+    id("checkstyle")
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("jacoco")
 }
 
 group = "hexlet.code"
@@ -9,11 +15,27 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("hexlet.code.App")
+}
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.13.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
 }
