0953af9fd19e6fd40135425e3bd7b798e5584d26
DashProsh <dash.prosh.am@gmail.com>
2025-02-12 15:10:59 +0200

    added filepath1, filepath2, --format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,9 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.io.File;
+import java.util.List;
+
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.1.1",
         description = "Compares two configuration files and shows a difference.")
@@ -16,9 +19,19 @@ class App implements Runnable {
     @Option(names = { "-V", "--version" }, versionHelp = true, description = "Print version information and exit.")
     boolean versionInfoRequested;
 
+    @Option(names = { "-f", "--format " }, paramLabel = "format", description = "output format [default: stylish]", defaultValue = "stylish")
+    private String format;
+
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+
     @Override
     public void run () {
-        System.out.println("Use -h or --help to see available options.");
+        System.out.println("0");
     }
 
     public static void main(String[] args) {
