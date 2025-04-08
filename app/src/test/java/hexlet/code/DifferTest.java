894d7a278c21feaba16b651893c3671fd4e1090b
DashProsh <dash.prosh.am@gmail.com>
2025-04-08 18:38:23 +0300

    added other tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import hexlet.code.formatters.Formatter;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,36 +15,76 @@ class DifferTest {
         return Files.readString(path).strip();
     }
 
+    private static String getFixturePath(String fileName) {
+        return Paths.get("src/test/resources/fixtures/" + fileName).toString();
+    }
+
+
     @Test
-    void testGenerateDiffForJsonStylish() throws IOException {
-        var data1 = Parser.parseFromResources("file1.json");
-        var data2 = Parser.parseFromResources("file2.json");
-        var diff = Differ.calculateDiff(data1, data2);
-        var actual = Formatter.format(diff, "stylish");
-        assertEquals(readExpected("expectedStylish.txt"), actual.strip());
-        System.out.println("ВСЕ РАБОТАЕТ!!!");
+    void testGenerateJsonStylish() throws IOException {
+        String expected = readExpected("expectedStylish.txt");
+        String actual = Differ.generate(getFixturePath("file1.json"), getFixturePath("file2.json"), "stylish");
+        assertEquals(expected.strip(), actual.strip());
+        System.out.println("1 ВСЕ РАБОТАЕТ");
     }
 
     @Test
-    void testGenerateDiffForJsonPlain() throws IOException {
-        var data1 = Parser.parseFromResources("file1.yml");
-        var data2 = Parser.parseFromResources("file2.yml");
-        var diff = Differ.calculateDiff(data1, data2);
-        var actual = Formatter.format(diff, "plain");
-        assertEquals(readExpected("expectedPlain.txt"), actual.strip());
-        System.out.println("ВСЕ РАБОТАЕТ! лала!!!");
+    void testGenerateJsonPlain() throws IOException {
+        String expected = readExpected("expectedPlain.txt");
+        String actual = Differ.generate(getFixturePath("file1.json"), getFixturePath("file2.json"), "plain");
+        assertEquals(expected.strip(), actual.strip());
+        System.out.println("2 ВСЕ РАБОТАЕТ");
     }
 
     @Test
-    void testGenerateDiffForJsonJson() throws IOException {
-        var data1 = Parser.parseFromResources("file1.json");
-        var data2 = Parser.parseFromResources("file2.json");
-        var diff = Differ.calculateDiff(data1, data2);
-        var actual = Formatter.format(diff, "json");
+    void testGenerateJsonJson() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         var expected = mapper.readTree(readExpected("expectedJson.json"));
-        var actualTree = mapper.readTree(actual);
-        assertEquals(expected, actualTree);
-        System.out.println("ВСЕ РАБОТАЕТ! дада!!!");
+        var actual = mapper.readTree(Differ.generate(getFixturePath("file1.json"), getFixturePath("file2.json"), "json"));
+        assertEquals(expected, actual);
+        System.out.println("3 ВСЕ РАБОТАЕТ");
+    }
+
+
+    @Test
+    void testGenerateYamlStylish() throws IOException {
+        String expected = readExpected("expectedStylish.txt");
+        String actual = Differ.generate(getFixturePath("file1.yml"), getFixturePath("file2.yml"), "stylish");
+        assertEquals(expected.strip(), actual.strip());
+        System.out.println("4 ВСЕ РАБОТАЕТ");
+    }
+
+    @Test
+    void testGenerateYamlPlain() throws IOException {
+        String expected = readExpected("expectedPlain.txt");
+        String actual = Differ.generate(getFixturePath("file1.yml"), getFixturePath("file2.yml"), "plain");
+        assertEquals(expected.strip(), actual.strip());
+        System.out.println("5 ВСЕ РАБОТАЕТ");
+    }
+
+    @Test
+    void testGenerateYamlJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        var expected = mapper.readTree(readExpected("expectedJson.json"));
+        var actual = mapper.readTree(Differ.generate(getFixturePath("file1.yml"), getFixturePath("file2.yml"), "json"));
+        assertEquals(expected, actual);
+        System.out.println("6 ВСЕ РАБОТАЕТ");
+    }
+
+    @Test
+    void testGenerateJsonDefault() throws IOException {
+        String expected = readExpected("expectedStylish.txt");
+        String actual = Differ.generate(getFixturePath("file1.json"), getFixturePath("file2.json"));
+        assertEquals(expected.strip(), actual.strip());
+        System.out.println("7 ВСЕ РАБОТАЕТ");
+    }
+
+
+    @Test
+    void testGenerateYamlDefault() throws IOException {
+        String expected = readExpected("expectedStylish.txt");
+        String actual = Differ.generate(getFixturePath("file1.yml"), getFixturePath("file2.yml"));
+        assertEquals(expected.strip(), actual.strip());
+        System.out.println("8 ВСЕ РАБОТАЕТ");
     }
 }
